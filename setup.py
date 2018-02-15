e8f5eb18ad0d02dc13e124c008ad9f6e0bae8e13
fedetorre <fede.torre@gmail.com>
2018-02-15 12:58:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyranch',
+    version = '0.1dev',
+    packages = ['pyranch',],
+    author = '',
+    author_email = '',
+    url = 'https://github.com/fedetorre/pyranch',
+    license = '',
+    long_description = open('README.md').read(),
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
