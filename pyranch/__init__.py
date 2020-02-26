8a5a6b96a560c6c50f2c3d2c0cdfa6c8e6266e82
Federico Torresan <federico.torresan@quentral.com>
2020-02-26 11:41:44 +0100

    perf: Update dependencies and setup workflow for publication on pypi

--- pyranch/__init__.py
+++ pyranch/__init__.py
@@ -1,2 +1,6 @@
 from .exceptions import APIException, JSONDecodeError, NoTokenProvided, ResponseError, RequestError
 from .cattle import Environment
+
+__title__ = "pyranch"
+__version__ = "1.4.4"
+__author__ = "Federico Torresan"
--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+
+def get_version():
+    with open("pyranch/__init__.py") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1])
+
+
 setup(
     name = 'pyranch',
-    version = '0.1dev',
-    packages = ['pyranch',],
-    author = '',
-    author_email = '',
+    version = get_version(),
+    packages = find_packages(),
+    author = 'Federico Torresan',
+    author_email = 'federico.torresan@quentral.com',
     url = 'https://github.com/fedetorre/pyranch',
-    license = '',
+    license = 'LGPLv3',
     install_requires=required,
     long_description = open('README.md').read(),
     classifiers = [
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.2',
         'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ]
 )
